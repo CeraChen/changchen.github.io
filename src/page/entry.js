ff7cf7cdbe9372fade204579220fff53fa22cb37
CeraChen <1059315177@qq.com>
2024-09-29 21:55:53 +0800

    update

--- src/page/entry.js
+++ src/page/entry.js
@@ -69,6 +69,11 @@ function EntryPage( ) {
 
     return (        
         <div className="main">
+            <div className="top-border"></div>
+            <div className="top-flowers">
+                <span className="floating-left">✢°. ✤  </span>
+                <span className="floating-right">.*❈。✢</span>
+            </div>
             <div className="canvas">
                 <div className="profile-container">
                     <div className="fixed-area">
